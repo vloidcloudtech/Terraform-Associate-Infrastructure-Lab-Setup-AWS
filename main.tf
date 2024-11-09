d77333ee97e5a5f190d9e8ff054bac986247db96
vloidcloudtech <115376153+vloidcloudtech@users.noreply.github.com>
2024-11-08 21:06:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -217,23 +217,7 @@ resource "aws_subnet" "variables-subnet" {
   }
 }
 
-resource "tls_private_key" "generated" {
-  algorithm = "RSA"
-}
 
-resource "local_file" "private_key_pem" {
-  content  = tls_private_key.generated.private_key_pem
-  filename = "MyAWSKey.pem"
-}
-
-resource "aws_key_pair" "generated" {
-  key_name   = "MyAWSKey"
-  public_key = tls_private_key.generated.public_key_openssh
-
-  lifecycle {
-    ignore_changes = [key_name]
-  }
-}
 # Security Groups
 
 resource "aws_security_group" "ingress-ssh" {
